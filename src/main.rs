4d86920882f1f3df633521c81877b72ade94a1ca
BigFish2086 <a8686.ibrahim@gmail.com>
2024-04-14 17:28:45 +0200

    add parsing for some of reddis RESP messages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+mod parser;
+
 use std::env;
 use std::net::TcpListener;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let port = match args.len() {
-        2 if args[0] == "--port".to_string() => {
-            args[1].trim().parse::<u16>().expect("ERROR: expected port to be 0-65535")
-        }
+        2 if args[0] == "--port".to_string() => args[1]
+            .trim()
+            .parse::<u16>()
+            .expect("ERROR: expected port to be 0-65535"),
         _ => 6379,
     };
 
---
+++ src/parser.rs
@@ -0,0 +1,130 @@
+const CR: u8 = b'\r';
+const LF: u8 = b'\n';
+
+// https://redis.io/docs/reference/protocol-spec
+
+#[derive(Debug, PartialEq)]
+pub enum RESPOutput {
+    SimpleString(String),
+    SimpleError(String),
+    Integer(i64),
+    BulkString(String),
+    Array(Vec<RESPOutput>),
+    Null,
+}
+
+#[derive(Debug, PartialEq, thiserror::Error)]
+pub enum ParseError {
+    #[error("ERROR: Invalid Input")]
+    InvalidInput,
+    #[error("ERROR: Incomplete Input")]
+    IncompleteInput,
+    #[error("ERROR: CRLF Not Found")]
+    CRLFNotFound,
+    #[error("ERROR: Unkonwn Symbol")]
+    UnkownSymbol,
+}
+
+pub type ParseResult<'a> = std::result::Result<(RESPOutput, &'a [u8]), ParseError>;
+pub type ParseCRLFResult<'a> = std::result::Result<(&'a [u8], &'a [u8]), ParseError>;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn parse_resp<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        if input.len() == 0 || input[0] == 0 {
+            return Err(ParseError::IncompleteInput);
+        }
+        let symbol = String::from_utf8_lossy(&input[0..1]);
+        let symbol = symbol.as_ref();
+        let remaining = &input[1..];
+        match symbol {
+            "+" => Self::parse_simple_string(remaining),
+            "-" => Self::parse_simple_error(remaining),
+            "$" => Self::parse_bulk_string(remaining),
+            ":" => Self::parse_integer(remaining),
+            "*" => Self::parse_array(remaining),
+            _ => Err(ParseError::UnkownSymbol),
+        }
+    }
+
+    pub fn parse_simple_string<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        Self::parse_until_crlf(input).map(|(result, remaining)| {
+            let output = String::from(String::from_utf8_lossy(result));
+            (RESPOutput::SimpleString(output), remaining)
+        })
+    }
+
+    pub fn parse_simple_error<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        Self::parse_until_crlf(input).map(|(result, remaining)| {
+            let output = String::from(String::from_utf8_lossy(result));
+            (RESPOutput::SimpleString(output), remaining)
+        })
+    }
+
+    pub fn parse_bulk_string<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        let (data_len, remaining) = Self::parse_until_crlf(input)?;
+        let data_len = String::from(String::from_utf8_lossy(data_len));
+        if data_len == "-1" {
+            return Ok((RESPOutput::Null, "".as_bytes()));
+        }
+        match data_len.parse::<usize>() {
+            Ok(data_len) => {
+                let (data, remaining) = Self::parse_until_crlf(remaining)?;
+                if data.len().lt(&data_len) {
+                    Err(ParseError::IncompleteInput)
+                } else if data.len().gt(&data_len) {
+                    Err(ParseError::InvalidInput)
+                } else {
+                    let data = String::from(String::from_utf8_lossy(data));
+                    Ok((RESPOutput::BulkString(data), remaining))
+                }
+            }
+            Err(_) => Err(ParseError::InvalidInput),
+        }
+    }
+
+    pub fn parse_integer<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        let (result, remaining) = Self::parse_until_crlf(input)?;
+        let result = String::from(String::from_utf8_lossy(result));
+        match result.parse::<i64>() {
+            Ok(num) => Ok((RESPOutput::Integer(num), remaining)),
+            Err(_) => Err(ParseError::InvalidInput),
+        }
+    }
+
+    pub fn parse_array<'a>(input: &'a [u8]) -> ParseResult<'a> {
+        let (data_len, mut remaining) = Self::parse_until_crlf(input)?;
+        let data_len = String::from(String::from_utf8_lossy(data_len));
+        if data_len == "-1" {
+            return Ok((RESPOutput::Null, "".as_bytes()));
+        }
+        match data_len.parse::<usize>() {
+            Ok(data_len) => {
+                let mut elements: Vec<RESPOutput> = Vec::new();
+                for _ in 0..data_len {
+                    let (el, rem) = Self::parse_resp(remaining)?;
+                    elements.push(el);
+                    remaining = rem;
+                }
+                Ok((RESPOutput::Array(elements), remaining))
+            }
+            Err(_) => Err(ParseError::InvalidInput),
+        }
+    }
+
+    pub fn parse_until_crlf<'a>(input: &'a [u8]) -> ParseCRLFResult<'a> {
+        if input.len() == 0 {
+            return Ok((&[0], &[0]));
+        }
+        for i in 0..input.len() - 1 {
+            if input[i] == 0 {
+                return Ok((&[0], &[0]));
+            }
+            if input[i] == CR && input[i + 1] == LF {
+                return Ok((&input[0..i], &input[i + 2..]));
+            }
+        }
+        Err(ParseError::CRLFNotFound)
+    }
+}
